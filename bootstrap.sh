1553f2c2316dec1730894d74da4a5b84e519e3df
Trenton Potgieter <darkreapyre@gmail.com>
2017-01-09 14:36:31 -0800

    Initial configuration for Local version of Hybrid

---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+HOME=/vagrant
+
+echo "Bootstrapping Admin Node"
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install git curl zip unzip wget acl software-properties-common
+
+# Install Vagrant
+echo "Installing Vagrant"
+##sudo apt-get -y install vagrant
+wget https://releases.hashicorp.com/vagrant/1.8.7/vagrant_1.8.7_x86_64.deb
+sudo dpkg -i vagrant_1.8.7_x86_64.deb
+#vagrant plugin install vagrant-address
+vagrant plugin install vagrant-aws
+
+# Install Ansible
+echo "Installing Ansible"
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get -y update
+sudo apt-get -y install ansible
+
+# To ensure permission issues with nested Vagrant
+cp -R /vagrant/provision /home/vagrant/
+echo "Bootstrap Complete"
+
+# Provision
+echo "Deploy Data Science admin node"
+cd /home/vagrant/provision
+ansible-playbook local.yml --extra-vars "@init.yml"
+#vagrant up --no-parallel
