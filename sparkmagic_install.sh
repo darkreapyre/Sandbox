907c8dd4d0ae3ef5341a3b79479616a735862622
Trenton Potgieter <darkreapyre@gmail.com>
2017-03-15 08:58:18 -0700

    updated sparkmagic installation for linux

--- sparkmagic_install.sh
+++ sparkmagic_install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Install sparkmagic
-pip install sparkmagic
+#pip install sparkmagic #<-- Should alredy be installed in the conda environment.yml
 
 # Ensure ipywidget is installed
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
 # Add the necessary Jupyter kernels
-jupyter-kernelspec install /d/users/auxadmin/appdata/local/continuum/miniconda3/envs/sparkaas/lib/site-packages/sparkmagic/kernels/sparkkernel
-jupyter-kernelspec install /d/users/auxadmin/appdata/local/continuum/miniconda3/envs/sparkaas/lib/site-packages/sparkmagic/kernels/pysparkkernel
-jupyter-kernelspec install /d/users/auxadmin/appdata/local/continuum/miniconda3/envs/sparkaas/lib/site-packages/sparkmagic/kernels/sparkrkernel
+jupyter-kernelspec install /home/ubuntu/miniconda3/envs/SparkaaS/lib/python2.7/site-packages/sparkmagic/kernels/sparkkernel
+jupyter-kernelspec install /home/ubuntu/miniconda3/envs/SparkaaS/lib/python2.7/site-packages/sparkmagic/kernels/pysparkkernel
+jupyter-kernelspec install /home/ubuntu/miniconda3/envs/SparkaaS/lib/python2.7/site-packages/sparkmagic/kernels/sparkrkernel
 
 # Enable Server Extensions
 jupyter serverextension enable --py sparkmagic
