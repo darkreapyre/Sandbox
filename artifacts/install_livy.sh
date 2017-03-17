ecd6b4c728d95f58149641dc36f910a46f150a01
Trenton Potgieter <darkreapyre@gmail.com>
2017-03-17 10:19:42 -0700

    removed nohup

--- artifacts/install_livy.sh
+++ artifacts/install_livy.sh
@@ -3,6 +3,7 @@
 aws s3 cp s3://chkrd/artifacts/livy-server-0.3.0.zip /home/hadoop/
 unzip /home/hadoop/livy-server-0.3.0.zip
 rm /home/hadoop/livy-server-0.3.0.zip
+mkdir /home/hadoop/livy-server-0.3.0/logs
 
 # Copy Configuration files
 aws s3 cp s3://chkrd/artifacts/livy.conf /home/hadoop/livy-server-0.3.0/conf/
