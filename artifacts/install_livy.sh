b7f92041f17b1d7bc3e42a05a5d38aad8b135fef
Trenton Potgieter <darkreapyre@gmail.com>
2017-03-17 10:08:11 -0700

    removed nohup

--- artifacts/install_livy.sh
+++ artifacts/install_livy.sh
@@ -9,4 +9,5 @@ aws s3 cp s3://chkrd/artifacts/livy.conf /home/hadoop/livy-server-0.3.0/conf/
 aws s3 cp s3://chkrd/artifacts/livy-env.sh /home/hadoop/livy-server-0.3.0/conf/
 
 # Start Livy
-nohup /home/hadoop/livy-server-0.3.0/bin/livy-server > /tmp/livy.log 2>&1 &
+#nohup /home/hadoop/livy-server-0.3.0/bin/livy-server > /tmp/livy.log 2>&1 &
+/home/hadoop/livy-server-0.3.0/bin/livy-server
